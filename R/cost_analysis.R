9ace0c17f79717228c984fb0e3bc6c66f7a3e4df
Stefan Eng <stefaneng@mednet.ucla.edu>
2022-07-11 11:58:46 -0700

    Update cost analysis axis titles and gotham font

--- R/cost_analysis.R
+++ R/cost_analysis.R
@@ -4,7 +4,12 @@
 #' @param extension the extension for the plot
 #'
 #' @export
-plot.study.hours <- function(cost.data, plot.path, extension, ...) {
+plot.study.hours <- function(
+  cost.data,
+  plot.path,
+  extension,
+  gotham.font = TRUE,
+  ...) {
   digitx.hours <- cbind(cost.data[, c('Patient', 'patient.id', 'Digitx.Time.total.hrs')], study = 1);
   colnames(digitx.hours) <- c('Patient', 'patient.id', 'hours', 'study');
   trad.hours <- cbind(cost.data[, c('Patient', 'patient.id', 'Traditional.Time.total.hrs')], study = 0);
@@ -17,18 +22,33 @@ plot.study.hours <- function(cost.data, plot.path, extension, ...) {
 
   time.df$phase0b <- ifelse(as.numeric(time.df$Patient >= 4), TRUE, FALSE);
 
-  create.boxplot(
+  height <- 6;
+  width <- 6;
+  filename <- file.path(
+    plot.path,
+    generate.filename('digIT-EX', file.core = 'phase0b_boxplot_study_hours', extension = extension)
+    );
+  print(sprintf('Plotting to: %s', filename));
+
+  hours.plot <- create.boxplot(
     formula = hours ~ as.factor(study),
     data = time.df[time.df$phase0b == 1, ],
     add.stripplot = TRUE,
-    ylab.label = 'Total Patient Hours',
+    ylab.label = 'Estimated Time Commitment, Hours',
     xlab.label = '',
     ylimits = c(-10, 175),
-    filename = file.path(
-      plot.path,
-      generate.filename('digIT-EX', file.core = 'phase0b_boxplot.study.hours', extension = extension)
-      ),
-      ...
+    ...
+    );
+
+  if (gotham.font) {
+    hours.plot <- replace.font(hours.plot, font = 'iCiel Gotham Medium');
+    }
+
+  BoutrosLab.plotting.general::write.plot(
+    trellis.object = hours.plot,
+    filename = filename,
+    height = height,
+    width = width
     );
   }
 
@@ -38,24 +58,45 @@ plot.study.hours <- function(cost.data, plot.path, extension, ...) {
 #' @param extension the extension for the plot
 #'
 #' @export
-plot.total.cost <- function(cost.data, plot.path, extension, ...) {
+plot.total.cost <- function(
+  cost.data,
+  plot.path,
+  extension,
+  gotham.font = TRUE,
+  ...) {
   phase.0b.costs <- cbind(cost.data[cost.data$phase0b, 'Total.Cost', drop = FALSE], group = 1);
   # Add the DigITx costs as '0'
   phase.0b.costs <- rbind(phase.0b.costs, c(0, 2));
 
+  height <- 6;
+  width <- 6;
+
+  filename <- file.path(
+    plot.path,
+    generate.filename('digIT-EX', file.core = 'phase0b_boxplot_total_cost', extension = extension)
+    );
+  print(sprintf('Plotting to: %s', filename));
+
   # phase.0b.costs
-  create.boxplot(
+  cost.plot <- create.boxplot(
     Total.Cost ~ as.factor(group),
     data = phase.0b.costs,
     add.stripplot = TRUE,
-    ylab.label = 'Estimated Patient Cost ($)',
+    ylab.label = 'Estimated Cost, $',
     xlab.label = '',
-    filename = file.path(
-      plot.path,
-      generate.filename('digIT-EX', file.core = 'phase0b_boxplot.total.cost', extension = extension)
-    ),
     ...
-  );
+    );
+
+  if (gotham.font) {
+    cost.plot <- replace.font(cost.plot, font = 'iCiel Gotham Medium');
+    }
+
+  BoutrosLab.plotting.general::write.plot(
+    trellis.object = cost.plot,
+    filename = filename,
+    height = height,
+    width = width
+    );
   }
 
 #' Full cost/hour analysis
