f9700a76ad7af311323643857cef0d8d203e1f6b
Stefan Eng <stefaneng@mednet.ucla.edu>
2023-04-18 15:45:20 -0700

    Update plot adherence from new phase1 patients

--- inst/plot_adherence.R
+++ inst/plot_adherence.R
@@ -2,7 +2,7 @@ library(EXONC.DEXP);
 library(BoutrosLab.plotting.general);
 
 script.name <- 'adherence';
-data.folder <- Sys.getenv('EXONC_DEXP_HOME');
+data.folder <- Sys.getenv('EXONC_HOME');
 if (data.folder == '') data.folder <- 'DEXP_results';
 
 analysis.init(
@@ -17,29 +17,55 @@ analysis.init(
     # Boxplot with the points shown (stripplot with a barplot overlaid), with columns for each data-type,
     # and then outlier points labeled with their patient numbers
     # Make a copy of the adherence data for Phase 0a patients
-    perc.phase0a.long <- read.table(system.file('extdata', 'adherence_perc_phase0a.tsv', package = 'EXONC.DEXP'), header = TRUE, sep = '\t');
-    perc.phase0b.long <- read.table(system.file('extdata', 'adherence_perc_phase0a.tsv', package = 'EXONC.DEXP'), header = TRUE, sep = '\t');
-
-    perc.phase0a.long <- perc.phase0a.long[perc.phase0a.long$Variable != 'Sleep', ];
-    perc.phase0b.long <- perc.phase0b.long[perc.phase0b.long$Variable != 'Sleep', ];
+    # perc.phase0a.long <- read.table(system.file('extdata', 'adherence_perc_phase0a.tsv', package = 'EXONC.DEXP'), header = TRUE, sep = '\t');
+    # perc.phase0b.long <- read.table(system.file('extdata', 'adherence_perc_phase0b.tsv', package = 'EXONC.DEXP'), header = TRUE, sep = '\t');
+    #
+    # perc.phase0a.long <- perc.phase0a.long[perc.phase0a.long$Variable != 'Sleep', ];
+    # perc.phase0b.long <- perc.phase0b.long[perc.phase0b.long$Variable != 'Sleep', ];
 
     variable.names <- c('Exercise\nTherapy', 'Watch', 'Blood\nPressure', 'Scale');
 
+    adherence.path <- file.path(data.folder, 'Phase1', 'raw_data', 'PRESTO Adherence_Phase0-1.xlsx')
+    adherence <- parse.adherence.xlsx(adherence.path)
+
+    write.table(
+      adherence,
+      file = file.path(data.folder, 'Phase1', 'processed_xlsx', 'PRESTO_Adherence_Phase0-1.tsv'),
+      sep = '\t',
+      row.names = FALSE
+      );
+
+    adherence.long <- reshape(
+      data = adherence[, c('study.id', 'phase', 'attendance.percent', 'watch.percent', 'bp.percent', 'scale.percent')],
+      idvar = c("study.id", 'phase'),
+      varying = c("attendance.percent", "watch.percent", "bp.percent", "scale.percent"),
+      v.names = "Percent",
+      timevar = "variable",
+      times = c("attendance", "watch", "bp", "scale"),
+      direction = "long"
+      );
+
+    adherence.long$Variable.factor <- factor(adherence.long$variable, levels = c('attendance', 'watch', 'bp', 'scale'));
+    adherence.long$Percent <- adherence.long$Percent * 100;
+
+    adherence.long.0b <- adherence.long[startsWith(adherence.long$phase, '0B'), ];
+    adherence.long.phase1 <- adherence.long[startsWith(adherence.long$phase, '1a'), ];
+
     adherence.boxplot(
-      x = perc.phase0a.long,
+      x = adherence.long.0b,
       plot.path = plot.path,
       extension = extension,
-      phase = 'phase0a',
-      use.gotham.font = TRUE,
+      phase = 'phase0b',
+      use.gotham.font = FALSE,
       variable.names = variable.names
       );
 
     adherence.boxplot(
-      x = perc.phase0b.long,
+      x = adherence.long.phase1,
       plot.path = plot.path,
       extension = extension,
-      phase = 'phase0b',
-      use.gotham.font = TRUE,
+      phase = 'phase1',
+      use.gotham.font = FALSE,
       variable.names = variable.names
       );
     }
